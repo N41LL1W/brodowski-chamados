8dca88a93253512a519b9ee5a055e0b2d4a18232
N41LL1W <willian.play.cel@gmail.com>
2026-02-10 16:21:10 -0300

    fix: permite domínios externos para carregamento de imagens e atualiza rotas

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: false,
+  images: {
+    // Permite carregar imagens de qualquer lugar. 
+    // Ideal para quando você usa múltiplos serviços de upload ou URLs externas.
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // O asterisco duplo aceita todos os domínios
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
