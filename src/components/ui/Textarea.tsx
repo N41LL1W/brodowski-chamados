ab234b9f82049356df332242837c91446d3935d0
N41LL1W <willian.play.cel@gmail.com>
2026-03-19 12:32:05 -0300

    fix: TextArea v0.1

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -1,9 +1,7 @@
 export function Textarea({ className = "", ...props }: any) {
   return (
     <textarea
-      className={`w-full px-3 py-2 rounded-lg border border-gray-300 
-      dark:border-gray-700 dark:bg-gray-300 dark:text-gray-700
-      focus:ring-2 focus:ring-blue-500 focus:border-blue-500
+      className={`w-full px-3 py-2 rounded-lg border 
       outline-none transition-all ${className}`}
       {...props}
     />
