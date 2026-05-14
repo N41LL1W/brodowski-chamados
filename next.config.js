90c09737c0142058bfaf407c4a7bf77332331aad
N41LL1W <willian.play.cel@gmail.com>
2026-05-14 09:32:03 -0300

    fix: remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
-    buildExcludes: [/middleware-manifest\.json$/, /app-build-manifest\.json$/],
-    fallbacks: false,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     turbopack: {},
@@ -19,4 +10,4 @@ const nextConfig = {
     },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
